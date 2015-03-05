fb33339e2062028f4c61a1546f58133747ab3029
Shawn Zimmerman <nwahsadude@gmail.com>
2015-03-05 13:25:13 -0500

    Adding health hud item

--- public/js/entities/HUD.js
+++ public/js/entities/HUD.js
@@ -16,6 +16,7 @@ game.HUD.Container = me.Container.extend({
         this.name = "HUD";
 
         this.addChild(new game.HUD.ScoreItem(630, 440));
+        this.addChild(new game.HUD.HealthItem(630, 40));
     }
 });
 
@@ -39,5 +40,28 @@ game.HUD.ScoreItem = me.Renderable.extend({
 
     draw: function(renderer){
         this.font.draw(renderer, game.data.score, this.pos.x, this.pos.y);
+    }
+});
+
+game.HUD.HealthItem = me.Renderable.extend({
+    init: function(x, y){
+        this._super(me.Renderable, 'init' [x, y, 10, 10]);
+
+        this.font = new me.BitmapFont("32x32_font", 32);
+        this.font.set("left");
+
+        this.health = -1;
+    },
+
+    update: function(dt){
+        if (this.health !== game.data.health){
+            this.health = game.data.health;
+            return true;
+        }
+        return false
+    },
+
+    draw: function(renderer){
+        this.font.draw(renderer, game.data.health, this.pos.x, this.pos.y);
     }
 });
